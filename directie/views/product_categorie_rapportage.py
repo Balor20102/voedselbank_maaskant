bf4199f1261824d6d38d05cca348d18acaa0e3bc
DanielKool <daniel@familiekool.nl>
2023-12-06 10:26:29 +0100

    wip styling

--- directie/views/product_categorie_rapportage.py
+++ directie/views/product_categorie_rapportage.py
@@ -1,12 +1,14 @@
 # views.py
 from django.views import View
+from django.db.models import Count
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from magazijn.models import ProductItem
+from magazijn.models import ProductItem, Product
 from directie.filters import DateFilter
 
 class PCRapportage(LoginRequiredMixin, View):
     product_item_model = ProductItem
+    product_model = Product
     date_filter = DateFilter
     template_name = 'directie/product-categorie-rapportage.html'
 
@@ -18,9 +20,16 @@ class PCRapportage(LoginRequiredMixin, View):
         # Use the filtered queryset
         product_items = date_filter.qs
 
+        product_counts = product_items.values('product').annotate(count=Count('product'))
+
+        for product_count in product_counts:
+            product = self.product_model.objects.get(pk=product_count['product'])
+            product_count['product'] = product
+
         context = {
             'product_items': product_items,
             'date_filter': date_filter,
+            'product_counts': product_counts,
         }
 
         return render(request, self.template_name, context)
