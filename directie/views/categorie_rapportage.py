2154b8a5287f5193d6c142a573b3ec24b41923c6
DanielKool <daniel@familiekool.nl>
2023-12-07 08:56:19 +0100

    tussen pagina done and some css fixes

--- directie/views/categorie_rapportage.py
+++ directie/views/categorie_rapportage.py
@@ -1,8 +1,7 @@
 from django.views import View
-
 from django.shortcuts import render, redirect
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from magazijn.models import Catagorie
 
@@ -14,8 +13,12 @@ class CategorieRapportage(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
 
         catagorieën = Catagorie.objects.all()
+        paginator = Paginator(catagorieën, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         context = {
-            'catagorieen': catagorieën
+            'page_obj': page_obj
         }
         
         return render(request, self.template_name, context)
